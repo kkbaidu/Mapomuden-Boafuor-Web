716fc34b65ff57ad329993d87ba8970ebe0a6816
kkbaidu <kingsleybaidu99@gmail.com>
2025-09-16 18:41:23 +0000

    modify eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,8 @@ const eslintConfig = [
     rules: {
       "react/no-unescaped-entities": "off",
       "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ];
